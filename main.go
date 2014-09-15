baabaac2ec6fbfe8b67c2970b14b7d6a9e935bc4
Joao Sa <me@joaomsa.com>
2014-09-15 01:09:55 -0300

    Split resources into a separate file

--- main.go
+++ main.go
@@ -5,122 +5,8 @@ import (
 	"gopkg.in/yaml.v1"
 	"io/ioutil"
 	"log"
-	"os/exec"
 	"sync"
 )
-
-type Resource struct {
-	when   string
-	Start  string
-	Stop   string
-	Status string
-}
-
-func (r *Resource) SetYAML(tag string, data interface{}) bool {
-	when, ok := data.(map[interface{}]interface{})["when"].(string)
-	if ok {
-		switch when {
-		case "always", "charging", "discharging", "never":
-			r.when = when
-		default:
-			log.Fatalf("\"%v\" not a valid value for \"when\"", when)
-		}
-	} else {
-		r.when = "charging"
-	}
-
-	start, ok := data.(map[interface{}]interface{})["start"].(string)
-	if !ok {
-		log.Fatalf("Failed to define start command")
-	}
-	r.Start = start
-
-	stop, ok := data.(map[interface{}]interface{})["stop"].(string)
-	if !ok {
-		log.Fatalf("Failed to define stop command")
-	}
-	r.Stop = stop
-
-	status, ok := data.(map[interface{}]interface{})["status"].(string)
-	if !ok {
-		log.Fatalf("Failed to define status command")
-	}
-	r.Status = status
-
-	return true
-}
-
-func (r *Resource) WhenPlugged() bool {
-	switch r.when {
-	case "always", "charging":
-		return true
-	}
-	return false
-}
-
-func (r *Resource) WhenUnplugged() bool {
-	switch r.when {
-	case "always", "discharging":
-		return true
-	}
-	return false
-}
-
-func (r Resource) GetStatus() error {
-	cmd := exec.Command("bash", "-c", r.Status)
-	log.Printf("[status] %v\n", r.Status)
-	return cmd.Run()
-}
-
-func (r Resource) RunStart() error {
-	cmd := exec.Command("bash", "-c", r.Start)
-	log.Printf("[start] %v\n", r.Start)
-	return cmd.Start()
-}
-
-func (r Resource) RunStop() error {
-	cmd := exec.Command("bash", "-c", r.Stop)
-	log.Printf("[stop] %v\n", r.Stop)
-	return cmd.Start()
-}
-
-func (r Resource) Plug() {
-	status := r.GetStatus()
-	if status != nil {
-		if r.WhenPlugged() {
-			err := r.RunStart()
-			if err != nil {
-				log.Printf("[%v] %v", r.Start, err.Error())
-			}
-		}
-	} else {
-		if !r.WhenPlugged() {
-			err := r.RunStop()
-			if err != nil {
-				log.Printf("[%v] %v", r.Stop, err.Error())
-			}
-		}
-	}
-}
-
-func (r Resource) Unplug() {
-	status := r.GetStatus()
-	if status != nil {
-		if r.WhenUnplugged() {
-			err := r.RunStart()
-			if err != nil {
-				log.Printf("[%v] %v", r.Start, err.Error())
-			}
-		}
-	} else {
-		if !r.WhenUnplugged() {
-			err := r.RunStop()
-			if err != nil {
-				log.Printf("[%v] %v", r.Stop, err.Error())
-			}
-		}
-	}
-}
 
 func checkOnBattery() bool {
 	conn, connErr := dbus.SystemBus()
@@ -185,7 +71,6 @@ func main() {
 	}
 
 	resources := make(map[string]Resource)
-
 	yamlErr := yaml.Unmarshal([]byte(resourceData), resources)
 	if yamlErr != nil {
 		log.Fatalf("error: %v", yamlErr)
---
+++ resource.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+)
+
+type Resource struct {
+	when      string
+	startCmd  string
+	stopCmd   string
+	statusCmd string
+}
+
+func (r *Resource) SetYAML(tag string, data interface{}) bool {
+	when, ok := data.(map[interface{}]interface{})["when"].(string)
+	if ok {
+		switch when {
+		case "always", "charging", "discharging", "never":
+			r.when = when
+		default:
+			log.Fatalf("\"%v\" not a valid value for \"when\"", when)
+		}
+	} else {
+		r.when = "charging"
+	}
+
+	start, ok := data.(map[interface{}]interface{})["start"].(string)
+	if !ok {
+		log.Fatalf("Failed to define start command")
+	}
+	r.startCmd = start
+
+	stop, ok := data.(map[interface{}]interface{})["stop"].(string)
+	if !ok {
+		log.Fatalf("Failed to define stop command")
+	}
+	r.stopCmd = stop
+
+	status, ok := data.(map[interface{}]interface{})["status"].(string)
+	if !ok {
+		log.Fatalf("Failed to define status command")
+	}
+	r.statusCmd = status
+
+	return true
+}
+
+func (r *Resource) WhenPlugged() bool {
+	switch r.when {
+	case "always", "charging":
+		return true
+	}
+	return false
+}
+
+func (r *Resource) WhenUnplugged() bool {
+	switch r.when {
+	case "always", "discharging":
+		return true
+	}
+	return false
+}
+
+func (r *Resource) Status() error {
+	cmd := exec.Command("bash", "-c", r.statusCmd)
+	log.Printf("[status] %v\n", r.statusCmd)
+	return cmd.Run()
+}
+
+func (r *Resource) Start() error {
+	cmd := exec.Command("bash", "-c", r.startCmd)
+	log.Printf("[start] %v\n", r.startCmd)
+	err := cmd.Start()
+	if err != nil {
+		log.Printf("[%v] %v", r.startCmd, err.Error())
+	}
+	return err
+}
+
+func (r *Resource) Stop() error {
+	cmd := exec.Command("bash", "-c", r.stopCmd)
+	log.Printf("[stop] %v\n", r.stopCmd)
+	err := cmd.Start()
+	if err != nil {
+		log.Printf("[%v] %v", r.stopCmd, err.Error())
+	}
+	return err
+}
+
+func (r *Resource) Plug() {
+	status := r.Status()
+	if status != nil {
+		if r.WhenPlugged() {
+			r.Start()
+		}
+	} else {
+		if !r.WhenPlugged() {
+			r.Stop()
+		}
+	}
+}
+
+func (r *Resource) Unplug() {
+	status := r.Status()
+	if status != nil {
+		if r.WhenUnplugged() {
+			r.Start()
+		}
+	} else {
+		if !r.WhenUnplugged() {
+			r.Stop()
+		}
+	}
+}
